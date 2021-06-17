398ca8aef6732d58b98b20d516a59b0c683e2427
John Fulton <john@techelevator.com>
2021-06-16 20:52:14 -0400

    09_Capstone/

---
+++ 09_Capstone/Capstone.Tests/ParentTest.cs
@@ -0,0 +1,42 @@
+﻿using Capstone.DAL;
+using Microsoft.Extensions.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Transactions;
+
+namespace Capstone.Tests
+{
+    [TestClass]
+    public class ParentTest
+    {
+        private TransactionScope trans;
+
+        protected string connectionString;
+        protected VenueDAO venueDAO;
+
+        public ParentTest()
+        {
+            // Get the connection string from the appsettings.json file
+            IConfigurationBuilder builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            IConfigurationRoot configuration = builder.Build();
+
+            connectionString = configuration.GetConnectionString("Project");
+        }
+
+        [TestInitialize]
+        public void Setup()
+        {
+            trans = new TransactionScope();
+            venueDAO = new VenueDAO(connectionString);
+        }
+
+        [TestCleanup]
+        public void Reset()
+        {
+            trans.Dispose();
+        }
+    }
+}
